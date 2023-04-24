c0cd7de952cc137d64ba95857319ff3d25072c13
computer-mann <hpsn16@gmail.com>
2023-04-24 10:59:31 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrivatePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
