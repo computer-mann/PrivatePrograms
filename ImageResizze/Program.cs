5451d6d653131e85b29fd40ea89c0bd81128b16e
computer-mann <hpsn16@gmail.com>
2023-06-25 07:56:12 +0000

    hahaha

--- ImageResizze/Program.cs
+++ ImageResizze/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var path = "C:\\Users\\hpsn1\\OneDrive\\Documents\\Projects\\dotnet\\PrivatePrograms\\ImageResizze";
+            var info = new FileInfo(path+"\\1299476.png");
+            using (Image image = Image.Load(info.FullName))
+            {
+                image.Mutate(x => x.Resize(image.Width / 2,image.Height/2));
+                image.Save(path + "\\imagesharp1.png");
+            }
+            Console.WriteLine(info.FullName);
         }
     }
 }
