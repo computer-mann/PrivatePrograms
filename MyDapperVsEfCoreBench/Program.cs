4a231ace9083436c66dccd719a0674838af9b45b
computer-mann <hpsn16@gmail.com>
2023-09-11 08:35:11 +0000

    done with me

---
+++ MyDapperVsEfCoreBench/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyDapperVsEfCoreBench
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    /* test ef core store procedure vs dapper
+     * test getting the connection string from ef core vs creating a manual one for dapper
+     * test raw queries in efcore vs dapper
+     */
+}
