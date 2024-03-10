75f8828ecffcbc64fb15c6a18a21c4ae1ea39ab4
computer-mann <hpsn16@gmail.com>
2024-03-10 13:52:11 +0000

    long time no code

--- AsyncEnumerableStreams/Program.cs
+++ AsyncEnumerableStreams/Program.cs
@@ -1,26 +1,31 @@
-﻿namespace AsyncEnumerableStreams
+﻿using System.Runtime.CompilerServices;
+
+namespace AsyncEnumerableStreams
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            
-            //await foreach(var num in ASynchStream())
+            //foreach (var num in await chronousStream())
             //{
             //    Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {num}");
             //}
-            foreach(var num in YieldStream())
+            await foreach (var num in ASynchStream(new CancellationTokenSource().Token))
             {
-               Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {num}");
+                Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {num}");
             }
+            //foreach(var num in YieldStream())
+            //{
+            //   Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {num}");
+            //}
         }
-        static async IAsyncEnumerable<int> ASynchStream()
+        static async IAsyncEnumerable<int> ASynchStream([EnumeratorCancellation]CancellationToken token)
         {
             Console.WriteLine("about to do it asynchronously stream");
             var listing = new List<int>();
             for (int i = 0; i < 10; i++)
             {
-                await Task.Delay(1000);
+                await Task.Delay(1000,token);
                 yield return i;
 
             }
--- MyDapperVsEfCoreBench/Program.cs
+++ MyDapperVsEfCoreBench/Program.cs
@@ -7,8 +7,8 @@
             Console.WriteLine("Hello, World!");
         }
     }
-    /* test ef core store procedure vs dapper
+    /* test ef core store procedure vs dapper stored procedure || dapper is faster
      * test getting the connection string from ef core vs creating a manual one for dapper
-     * test raw queries in efcore vs dapper
+     * test raw queries in efcore vs dapper || dapper is faster
      */
 }
