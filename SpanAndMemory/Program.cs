1e0a3f165199b2f24eca386cdc57d771d97d41ed
computer-mann <hpsn16@gmail.com>
2024-03-11 01:54:14 +0000

    black maradona

--- SpanAndMemory/Program.cs
+++ SpanAndMemory/Program.cs
@@ -6,7 +6,10 @@ namespace SpanAndMemory
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<SpanLearn>();
+             BenchmarkRunner.Run<SpanLearn>();
+            //var spanlearn = new SpanLearn();
+            //var list = spanlearn.RawNaiveGetColumnsFromCsvList();
+           // Console.WriteLine(list.First());
         }
     }
 }
--- SpanAndMemory/SpanLearn.cs
+++ SpanAndMemory/SpanLearn.cs
@@ -10,24 +10,50 @@ namespace SpanAndMemory
     [MemoryDiagnoser]
     public class SpanLearn
     {
+        private string Path = "C:\\Users\\hpsn1\\OneDrive\\Documents\\Projects\\dotnet\\PrivatePrograms\\SpanAndMemory\\passenger_output.csv";
         [Benchmark]
-        public void RawNaiveGetColumnsFromCsv()
+        public Ticket[] RawNaiveGetColumnsFromCsvArray()
         {
-            var range = Enumerable.Range(1, 567890);
-            foreach (var line in range)
+            
+            var csv = File.ReadLines(Path).Skip(2).ToArray();
+            var tickets = new Ticket[csv.Length];
+            for (int i = 0; i < csv.Length; i++)
             {
-                int mul=line*2;
+                var line = csv[i].Split("|");
+                var ticket_no = line[2].Trim();
+                var passenger_name = line[0].Trim();
+                var book_ref = line[1].Trim();
+                tickets[i]=new Ticket(passenger_name,ticket_no,book_ref);
             }
+            return tickets;
+        }
+        [Benchmark]
+        public List<Ticket> RawNaiveGetColumnsFromCsvList()
+        {
+
+            var csv = File.ReadLines(Path).Skip(2).ToList();
+            var tickets = new List<Ticket>();
+            foreach (var row in csv)
+            {
+                var line = row.Split("|");
+                var passenger_name = line[0].Trim();
+                var book_ref = line[1].Trim();
+                var ticket_no = line[2].Trim();
+                tickets.Add(new Ticket(passenger_name, ticket_no, book_ref));
+            }
+            return tickets;
         }
 
-        [Benchmark]
-        public void SpanOptimizedGetLinesFromCsv()
-        {
-            var range = Enumerable.Range(1, 324321231);
-            foreach (var line in range)
-            {
-                int mul = line * 2; 
-            }
-        }
+        //[Benchmark]
+        //public void SpanOptimizedGetLinesFromCsv()
+        //{
+        //    var range = Enumerable.Range(1, 324321231);
+        //    foreach (var line in range)
+        //    {
+        //        int mul = line * 2; 
+        //    }
+        //}
+
+        public record Ticket(string passenger_id,string ticket_no,string book_ref);
     }
 }
