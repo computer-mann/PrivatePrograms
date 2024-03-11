9a7679f3a8a27f02b800ebd11542916b431107c2
computer-mann <hpsn16@gmail.com>
2024-03-11 01:15:33 +0000

    tuff seed

--- SpanAndMemory/Program.cs
+++ SpanAndMemory/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace SpanAndMemory
+﻿using BenchmarkDotNet.Running;
+
+namespace SpanAndMemory
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BenchmarkRunner.Run<SpanLearn>();
         }
     }
 }
--- SpanAndMemory/SpanLearn.cs
+++ SpanAndMemory/SpanLearn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace SpanAndMemory
 {
-    internal class SpanLearn
+    [MemoryDiagnoser]
+    public class SpanLearn
     {
+        [Benchmark]
+        public void RawNaiveGetColumnsFromCsv()
+        {
+            var range = Enumerable.Range(1, 567890);
+            foreach (var line in range)
+            {
+                int mul=line*2;
+            }
+        }
+
+        [Benchmark]
+        public void SpanOptimizedGetLinesFromCsv()
+        {
+            var range = Enumerable.Range(1, 324321231);
+            foreach (var line in range)
+            {
+                int mul = line * 2; 
+            }
+        }
     }
 }
