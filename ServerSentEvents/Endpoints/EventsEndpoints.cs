4d947f4821386392017c9f43366918f174603013
computer-mann <hpsn16@gmail.com>
2024-03-26 09:01:36 +0000

    server sent events only have to do with the content type in the header, I assume text/event-stream

---
+++ ServerSentEvents/Endpoints/EventsEndpoints.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ServerSentEvents.Endpoints
+{
+    public static class EventsEndpoints
+    {
+        public static IEndpointRouteBuilder UseEventsEndpointRoutes(this IEndpointRouteBuilder builder)
+        {
+            builder.MapGet("/sse/{pathed}",async (HttpContext context,[FromRoute]string pathed) =>
+            {
+                context.Response.Headers.Append("Content-Type", "text/event-stream");
+
+                while (true)
+                {
+                    var json= JsonSerializer.Serialize(new {guid=Guid.NewGuid(),time=DateTime.Now,agent=pathed});
+                    await context.Response.WriteAsync($"data: {json}\n\n");
+                    await context.Response.Body.FlushAsync();
+                    await Task.Delay(3000);
+                }
+            });
+
+
+            builder.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var summaries = new[]
+           {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+            return builder;
+        }
+    }
+}
---
+++ ServerSentEvents/Program.cs
@@ -0,0 +1,44 @@
+
+using ServerSentEvents.Endpoints;
+
+namespace ServerSentEvents
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                });
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.UseCors();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEventsEndpointRoutes();
+            
+
+            app.Run();
+        }
+    }
+}
