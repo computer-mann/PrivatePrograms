25147c99670e48d70846d88cdec363ff69673db5
computer-mann <hpsn16@gmail.com>
2024-03-27 01:38:34 +0000

    gimme love

--- SpanAndMemory/SpanLearn.cs
+++ SpanAndMemory/SpanLearn.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using nietras.SeparatedValues;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,13 @@ namespace SpanAndMemory
     public class SpanLearn
     {
         public const string Path = "C:\\Users\\hpsn1\\OneDrive\\Documents\\Projects\\dotnet\\PrivatePrograms\\SpanAndMemory\\passenger_output.csv";
+        List<string> csv = File.ReadLines(Path).Skip(2).ToList();
         [Benchmark]
         public Ticket[] CsvParser_Naive()
         {
-            
-            var csv = File.ReadLines(Path);
             var csvCount = csv.Count();
             var tickets = new Ticket[csvCount];
-            for (int i = 2; i < csvCount; i++)
+            for (int i = 0; i < csvCount; i++)
             {
                 var line = csv.ElementAt(i).Split("|");
                 var ticket_no = line[2].Trim();
@@ -39,7 +39,6 @@ namespace SpanAndMemory
                  0qpzc0vu6tn0vuy9g3jy0arg9 | FCC5B7   | 0005432000302
              */
             //bookref=6,ticketno=13,passenger=25
-            var csv = File.ReadLines(Path).Skip(2);
             var csvCount = csv.Count();
             var tickets = new Ticket[csvCount];
             
@@ -57,10 +56,22 @@ namespace SpanAndMemory
         [Benchmark]
         public Ticket[] Sep_CsvParser()
         {
-            var csv = File.ReadLines(Path).Skip(2);
-            var csvCount = csv.Count();
-            var tickets = new Ticket[csvCount];
-            return tickets;
+            using var reader = Sep.New('|').Reader().FromFile(Path);
+            var tickets=new List<Ticket>();
+            
+            foreach(var row in reader)
+            {
+                
+                if (row.ColCount < 2)
+                {
+                    Console.WriteLine(row.ToString());
+                    continue;
+                }
+                tickets.Add(new Ticket(row[0].ToString(), row[1].ToString(), row[2].ToString()));
+                
+            }
+            
+            return tickets.ToArray();
         }
 
         public record Ticket(string passenger_id,string ticket_no,string book_ref);
