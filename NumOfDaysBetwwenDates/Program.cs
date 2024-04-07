9cf644fd4b3a96a7692fe88ce4c6c84d6086f40b
computer-mann <hpsn16@gmail.com>
2024-04-07 11:32:42 +0000

    3 months

--- NumOfDaysBetwwenDates/Program.cs
+++ NumOfDaysBetwwenDates/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             var diff = FindNumberOfDaysBetweenProvidedDates(DateOnly.Parse("14 09 2023"), DateOnly.Parse("12 01 2024"));
-            Console.WriteLine($"I did not touch real code for {diff} days"); //269 days
+            Console.WriteLine($"I did not touch real code for {diff} days"); //120 days
         }
 
         static int FindNumberOfDaysBetweenProvidedDates(DateOnly from, DateOnly to)
         {
-            return to.DayOfYear + from.DayOfYear;
+            return to.DayOfYear + (365-from.DayOfYear);
         }
     }
 }
