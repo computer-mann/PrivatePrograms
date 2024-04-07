4a0c055c097be4c580a6ea4396705d53403150fe
computer-mann <hpsn16@gmail.com>
2024-04-07 11:29:02 +0000

    making up for not coding for 269 days

---
+++ NumOfDaysBetwwenDates/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NumOfDaysBetwwenDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var diff = FindNumberOfDaysBetweenProvidedDates(DateOnly.Parse("14 09 2023"), DateOnly.Parse("12 01 2024"));
+            Console.WriteLine($"I did not touch real code for {diff} days"); //269 days
+        }
+
+        static int FindNumberOfDaysBetweenProvidedDates(DateOnly from, DateOnly to)
+        {
+            return to.DayOfYear + from.DayOfYear;
+        }
+    }
+}
