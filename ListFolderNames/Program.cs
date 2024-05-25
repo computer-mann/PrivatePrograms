83d14988c6b6b70404cc8722bfc4a8141b8f69f1
Prince Harry Nunoo <hpsn16@gmail.com>
2024-05-25 10:16:06 +0000

    Image resizing

---
+++ ListFolderNames/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace ListFolderNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder = "D:\\Projects\\dotnet\\Hubtel\\Hubtel60\\Hubtel-Library\\src";
+            var toReplace = "D:\\Documents";
+            var info = new DirectoryInfo(folder);
+            try
+            {
+                foreach (var file in info.EnumerateDirectories())
+                {
+                    Console.WriteLine($"{file.Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+
+
+        }
+    }
+}
