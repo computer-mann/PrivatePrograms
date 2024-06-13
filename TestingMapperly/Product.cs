860ebc133eb3cffd074b772fefc0f365ef51fb64
Prince Harry Nunoo <hpsn16@gmail.com>
2024-06-13 07:30:43 +0000

    mapperly

---
+++ TestingMapperly/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingMapperly
+{
+    internal class Product
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now.ToLocalTime();
+        public int CurrentQuantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Price: {Price}, CreatedAt: {CreatedAt}, CurrentQuantity: {CurrentQuantity}";
+        }
+    }
+}
---
+++ TestingMapperly/Program.cs
@@ -0,0 +1,31 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace TestingMapperly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dto=new ProductRequestDto()
+            {
+                Name = "Product 10",
+                Price = 100,
+                NumberOfItems = 10
+            };
+            var mapper = new ProductDtoToProductModelMapper();
+            var product = mapper.Map(dto);
+            
+            Console.WriteLine($"{product}");
+
+        }
+    }
+
+
+    [Mapper]
+    internal partial class ProductDtoToProductModelMapper
+    {
+        [MapProperty(nameof(ProductRequestDto.NumberOfItems), nameof(Product.CurrentQuantity))]
+        public partial Product Map(ProductRequestDto productRequestDto);
+        
+    }
+}
