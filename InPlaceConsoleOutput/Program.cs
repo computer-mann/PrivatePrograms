68bff0db3a31ecd88b3329963294fb0e48ab93b9
Prince Harry Nunoo <hpsn16@gmail.com>
2024-06-25 10:48:40 +0000

    code chef

---
+++ InPlaceConsoleOutput/Program.cs
@@ -0,0 +1,18 @@
+﻿using UuidExtensions;
+
+namespace InPlaceConsoleOutput
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var output = "Hello, World!";
+            //Console.WriteLine(output);
+            //await Task.Delay(1000);
+            //output = "Hello, World! (Updated)";
+            var key = Uuid7.Id25();
+            Console.WriteLine(key);
+            Console.WriteLine(key.Count());
+        }
+    }
+}
