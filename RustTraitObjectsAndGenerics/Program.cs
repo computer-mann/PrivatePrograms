75e89f3033d3a00b82f838b3e6151655e5eba008
Prince Harry Nunoo <hpsn16@gmail.com>
2024-06-30 04:37:13 +0000

    RustTraitObjectsAndGenerics

---
+++ RustTraitObjectsAndGenerics/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RustTraitObjectsAndGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var components = new Draw[] { new Circle(), new Button() };
+            var screen = new Screen();
+            foreach (var component in components) {
+                screen.display(component);
+            }
+        }
+    }
+    interface Draw {
+        void draw();
+    }
+    class Screen {
+        public void display(Draw d) {
+            d.draw();
+        }
+    }
+    class Circle : Draw {
+        public void draw() {
+            Console.WriteLine("Circle");
+        }
+    }
+    class Button: Draw {
+        public void draw() {
+            Console.WriteLine("Button");
+        }
+    }
+}
