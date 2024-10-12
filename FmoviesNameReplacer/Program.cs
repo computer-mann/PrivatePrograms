632d744515e9d0bd1b2b6c3384d2185819151033
Prince Harry Nunoo <princeharry.nunoo@hubtel.com>
2024-10-12 19:38:41 +0000

    methods galore

--- FmoviesNameReplacer/Program.cs
+++ FmoviesNameReplacer/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var folder = "D:\\Projects\\dotnet\\NunooCorp.IdentityServer.Admin\\src";
+            var folder = "D:\\Projects\\dotnet\\NunooCorp.IdentityServer.Admin\\tests";
             var toReplace = "Skoruba";
             var replaceWith = "PrinceHarry";
             var info = new DirectoryInfo(folder);
@@ -14,26 +14,8 @@
                 {
                     if (Directory.Exists(firstLevel.FullName))
                     {
-                        
-                        var secondInfo = new DirectoryInfo(firstLevel.FullName);
-                        foreach (var secondLevel in secondInfo.EnumerateFiles())
-                        {
-                            Console.WriteLine(secondLevel.Name);
-                            if (secondLevel.Name.StartsWith(toReplace) && secondLevel.Name.EndsWith(".csproj"))
-                            {
-                                
-                                var oldname = secondLevel.Name;
-                                secondLevel.MoveTo(secondLevel.FullName.Replace(toReplace, replaceWith));
-                                Console.WriteLine($"{oldname} -> {secondLevel.Name}");
-                            }
-                        }
-                        //var oldname = firstLevel.Name;
-                        //Console.WriteLine($"{firstLevel.Name}");
-                        //if (firstLevel.Name.StartsWith(toReplace))
-                        //{
-                        //    firstLevel.MoveTo(firstLevel.FullName.Replace(toReplace, replaceWith));
-                        //    Console.WriteLine($"{oldname} -> {firstLevel.Name}");
-                        //}
+                        ChangeFileNames(firstLevel, toReplace, replaceWith);
+                        //ChangeFolderNames(firstLevel, toReplace, replaceWith);
                     }
                     
                 }
@@ -44,5 +26,31 @@
             }
 
         }
+        public static void ChangeFolderNames(DirectoryInfo firstLevel,string toReplace,string replaceWith)
+        {
+            var oldname = firstLevel.Name;
+            Console.WriteLine($"{firstLevel.Name}");
+            if (firstLevel.Name.StartsWith(toReplace))
+            {
+                firstLevel.MoveTo(firstLevel.FullName.Replace(toReplace, replaceWith));
+                Console.WriteLine($"{oldname} -> {firstLevel.Name}");
+            }
+        }
+        public static void ChangeFileNames(DirectoryInfo firstLevel, string toReplace, string replaceWith)
+        {
+            var secondInfo = new DirectoryInfo(firstLevel.FullName);
+            foreach (var secondLevel in secondInfo.EnumerateFiles())
+            {
+                Console.WriteLine(secondLevel.Name);
+                if (secondLevel.Name.StartsWith(toReplace) && secondLevel.Name.EndsWith(".csproj"))
+                {
+
+                    var oldname = secondLevel.Name;
+                    secondLevel.MoveTo(secondLevel.FullName.Replace(toReplace, replaceWith));
+                    Console.WriteLine($"{oldname} -> {secondLevel.Name}");
+                }
+            }
+        }
     }
+
 }
